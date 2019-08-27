308d10884179771c680f5f38d0a7086f6a639109
GongJS <382663074@qq.com>
2019-08-27 18:24:08 +0800

    deploy

--- example/pages/upload/upload.demo.tsx
+++ example/pages/upload/upload.demo.tsx
@@ -1,10 +1,37 @@
-import React, { Fragment,} from 'react';
+import React, { Fragment, useState} from 'react';
 import ReactMarkdown from 'react-markdown'
+import CodeCard from '../../CodeCard'
+import Icon from '../../../lib/components/icon/Icon'
+import Upload from '../../../lib/components/upload/Upload'
 
+interface FileListProps {
+  name?: string
+  uid?: string
+  status?: string
+  url?: string
+}
 
 export default () => {
+  const [fileList, setFileList] = useState<FileListProps[]>([])
+  const onFileChange = (newFileList:FileListProps[]) => {
+    setFileList(newFileList)
+  }
   return <Fragment>
     <ReactMarkdown source={require('!!raw-loader!./upload.md').default} className="md" />
+    <CodeCard
+      title="照片墙"
+      summary="用户可以上传图片并在列表中显示缩略图。"
+      code={`
+      <Upload action={'http://127.0.0.1:7001/api/upload'} fileList={fileList} onFileChange={onFileChange}>
+        <Icon name="add_light" />
+      </Upload>
+      `
+      }
+    >
+     <Upload action={'http://localhost:3000/upload'} fileList={fileList} onFileChange={onFileChange}>
+       <Icon name="add_light" />
+     </Upload>
+    </CodeCard>
     <ReactMarkdown source={require('!!raw-loader!./api.md').default} className="md" />
   </Fragment>
 }
