77b4fd33efc8a4e13ea6994dae867ddbf84f9e84
GongJS <382663074@qq.com>
2020-03-10 17:53:57 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const {
   CheckerPlugin
 } = require('awesome-typescript-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
 
 function resolve(dir) {
   return path.join(__dirname, '', dir)
@@ -28,17 +30,23 @@ module.exports = {
         use: ['awesome-typescript-loader']
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, 
-      {
-        test: /\.s[ac]ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        test: /\.(sa|sc|c)ss$/,  // 可以打包后缀为sass/scss/css的文件
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+              hmr: devMode, // 仅dev环境启用HMR功能
+            },
+          },
+          'css-loader',
+          'sass-loader'
+        ],
       },
       {
         test: /\.svg$/,
         loader: 'svg-sprite-loader'
-      }, 
+      },
       {
         test: /\.(png|jpg|gif|jpeg)$/,
         use: {
@@ -49,6 +57,12 @@ module.exports = {
     ]
   },
   plugins: [
-    new CheckerPlugin()
+    new CheckerPlugin(),
+    new MiniCssExtractPlugin({
+      // 这里的配置和webpackOptions.output中的配置相似
+      // 即可以通过在名字前加路径，来决定打包后的文件存在的路径
+      filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
+      chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css',
+    })
   ]
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,22 @@
 const baseConfig = require('./webpack.config');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = Object.assign({}, baseConfig, {
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      }
+    },
+  },
   externals: {
     react: {
       commonjs: 'react',
