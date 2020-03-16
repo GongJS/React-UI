98d49b047a95e18cd059a3ad2ec5c82627324747
gongjs <382663074@qq.com>
2020-03-16 13:48:19 +0800

    优化button.scss代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,50 @@
-const path = require('path');
-const {
-  CheckerPlugin
-} = require('awesome-typescript-loader')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const devMode = process.env.NODE_ENV !== 'production'
+const path = require('path')
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
 function resolve(dir) {
   return path.join(__dirname, '', dir)
 }
 module.exports = {
   entry: {
-    index: './lib/index.tsx'
+    index: './lib/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
     library: 'R-UI',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
-      '@': path.resolve(__dirname, './lib')
-    }
+      '@': path.resolve(__dirname, './lib'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ['awesome-typescript-loader']
+        use: ['awesome-typescript-loader'],
       },
       {
-        test: /\.(sa|sc|c)ss$/,  // 可以打包后缀为sass/scss/css的文件
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: './',
-              hmr: devMode, // 仅dev环境启用HMR功能
-            },
-          },
-          'css-loader',
-          'sass-loader'
-        ],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.svg$/,
-        loader: 'svg-sprite-loader'
+        loader: 'svg-sprite-loader',
       },
       {
         test: /\.(png|jpg|gif|jpeg)$/,
         use: {
           loader: 'file-loader',
           options: {},
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
-  plugins: [
-    new CheckerPlugin(),
-    new MiniCssExtractPlugin({
-      // 这里的配置和webpackOptions.output中的配置相似
-      // 即可以通过在名字前加路径，来决定打包后的文件存在的路径
-      filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
-      chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css',
-    })
-  ]
+  plugins: [new CheckerPlugin()],
 }
